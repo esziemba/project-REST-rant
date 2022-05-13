70cd2561a172af982279d45a941cce54fda4b70c
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-13 11:49:30 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.use('/places', require('./contollers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
