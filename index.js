6ecdfa008e8e9a4bbabf61c9ff0f1760cab1b6b8
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-13 11:18:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
