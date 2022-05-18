d277104c124b7710a02d5d9cf65624d119d5aa0a
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-18 10:02:18 -0500

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,11 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /place')
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
