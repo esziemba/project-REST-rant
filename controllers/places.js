8a1db26610e0df14fbdbbe68b18436041fff346d
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-19 20:16:02 -0500

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -14,7 +14,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
 
   }
 })
@@ -43,6 +43,18 @@ router.get('/', (req, res) => {
 
 })
 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 
 
 module.exports = router
