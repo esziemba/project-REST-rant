8617420464293173c149f582525083f24206b482
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-26 09:43:13 -0500

    Addind a Schema for Mongo connection

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
+//middleware
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
+// routes
 app.use('/places', require('./controllers/places'))
-
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -20,6 +22,10 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// DB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 
 app.listen(process.env.PORT)
