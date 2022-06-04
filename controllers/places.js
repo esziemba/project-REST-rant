63ad7b9853ccd16ee0bf667fa3e14857be714560
esziemba <97561230+esziemba@users.noreply.github.com>
2022-06-04 15:30:48 -0500

    Fixed rant rave comments

--- controllers/places.js
+++ controllers/places.js
@@ -102,6 +102,28 @@ router.post('/:id/rant', (req, res) => {
   })
 })
 
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,20 @@ function show(data) {
       No Comments yet!
     </h3>
   )
+  if (data,data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 🥰'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   let rating = (
     <h3 className="inactive">
       Not yet rated
