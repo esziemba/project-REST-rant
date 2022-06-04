b945dfffe3d1327cb221b818688f02ab2c1d0fd2
esziemba <97561230+esziemba@users.noreply.github.com>
2022-06-04 14:52:27 -0500

    Populated data for star ratings

--- controllers/places.js
+++ controllers/places.js
@@ -41,17 +41,38 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
-
 
 router.get('/:id/rant', (req, res) => {
   res.render('places/rant', {'id':req.params.id})
@@ -80,8 +101,6 @@ router.post('/:id/rant', (req, res) => {
       res.render('error404')
   })
 })
-
-
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,21 +7,25 @@ function show(data) {
       No Comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
-    comments = data.place.comments.map(c => {
-      return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-        </div>
-      )
-    })
-  }
-
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+  )}
   return (
     <Def>
       <main>
@@ -34,8 +38,8 @@ function show(data) {
             </h3>
           </div>
           <div className="col-sm-6">
-            <h2>Ratings</h2>
-            <p>Not yet Rated</p>
+            <h2>Rating</h2>
+            {rating}
             <h2>
               Description
             </h2>
