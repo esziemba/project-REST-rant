7476a406646978c4abd3eefb808ac5fc76f3c05d
esziemba <97561230+esziemba@users.noreply.github.com>
2022-06-09 16:11:39 -0500

    revert all changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('*', (req, res) => {
 })
 
 // DB connection
-`mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})`
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
